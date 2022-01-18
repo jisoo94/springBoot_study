c955cc4ae3ef57b56416a180f5ac1a5962121a1e
jisoo94 <max5751@naver.com>
2022-01-18 11:43:29 +0900

    REST API day02 - 02

--- src/main/java/co/mr/restapi02/controller/AcademyController.java
+++ src/main/java/co/mr/restapi02/controller/AcademyController.java
@@ -4,10 +4,9 @@ import co.mr.restapi02.mapper.AcademyMapper;
 
 import co.mr.restapi02.model.Academy;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/academy")
@@ -18,7 +17,12 @@ public class AcademyController {
 
     @PostMapping("")
     // 파라미터를 reqyestBody로 전달하도록 설정 바디가 받고 아카데미에 넘김
-    public int post(@RequestBody Academy academy){
+    public int post(@RequestBody Academy academy) {
         return academyMapper.insert(academy);
+    }
+
+    @GetMapping("")
+    public List<Academy> getAcademies(){
+        return academyMapper.getAcademies();
     }
 }
